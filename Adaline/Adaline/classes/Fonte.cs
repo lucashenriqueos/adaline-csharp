6a167ba8944921ca3a3b9ef989ddd8b2178012ab
Lucas Henrique <lucashenriqueos92@gmail.com>
2016-10-08 00:41:27 -0300

    network traiining

---
+++ Adaline/Adaline/classes/Fonte.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adaline
+{
+    class Fonte
+    {
+        List<Letra> letras;
+
+        public Fonte()
+        {
+            Letras = new List<Letra>();
+        }
+
+        public Fonte(List<Letra> letra)
+        {
+            Letras = letra;
+        }
+
+        internal List<Letra> Letras
+        {
+            get
+            {
+                return letras;
+            }
+
+            set
+            {
+                letras = value;
+            }
+        }
+    }
+}
---
+++ Adaline/Adaline/classes/Letra.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adaline
+{
+    class Letra
+    {
+        int[] neuronios;
+        int[] pesos;
+
+        public Letra()
+        {
+            Neuronios = new int[64];
+            Pesos = new int[64];
+        }
+
+        public int[] Neuronios
+        {
+            get
+            {
+                return neuronios;
+            }
+
+            set
+            {
+                neuronios = value;
+            }
+        }
+
+        public int[] Pesos
+        {
+            get
+            {
+                return pesos;
+            }
+
+            set
+            {
+                pesos = value;
+            }
+        }
+    }
+}
